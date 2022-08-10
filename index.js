80dc4d2e9389aad0253fcd636916f88056a9fd31
Eduardo Lingan <elingan@gmail.com>
2022-08-10 16:53:14 +0200

    fix error absolute path 3

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ async function run() {
     const supabaseUrl = core.getInput('SUPABASE_URL') || process.env.SUPABASE_URL
     const supabaseAnonKey = core.getInput('SUPABASE_ANON_KEY') || process.env.SUPABASE_ANON_KEY
     core.debug(`bucketName: ${bucketName}`)
-    // const supabase = createClient(supabaseUrl, supabaseAnonKey)
-    // const { data: bucket, error: errGetBucket } = await supabase.storage.getBucket(bucketName)
-    // if (errGetBucket && !errGetBucket.message.includes('not found')) {
-    //   core.setFailed(errGetBucket.message)
-    //   return
-    // }
+    const supabase = createClient(supabaseUrl, supabaseAnonKey)
+    const { data: bucket, error: errGetBucket } = await supabase.storage.getBucket(bucketName)
+    if (errGetBucket && !errGetBucket.message.includes('not found')) {
+      core.setFailed(errGetBucket.message)
+      return
+    }
     // if (!bucket) {
     //   const { error: errCreateBucket } = await supabase.storage.createBucket(bucketName, {
     //     public: false
