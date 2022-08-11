fe6db5958be3f24d20fd29c880f542120ed7cf3d
Eduardo Lingan <elingan@gmail.com>
2022-08-11 23:16:50 +0200

    ajustes a la tarea del action

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const { createClient } = require('@supabase/supabase-js')
 
 async function run() {
   try {
-    const bucketName = core.getInput('bucket-name') || 'www-test'
-    const distFolder = core.getInput('dist-folder') || 'dist'
+    const bucketName = core.getInput('bucket-name')
+    const distFolder = core.getInput('dist-folder')
     const supabaseUrl = process.env.SUPABASE_URL
     const supabaseAnonKey = process.env.SUPABASE_ANON_KEY
     
@@ -21,7 +21,7 @@ async function run() {
     const { data: bucket, error: errGetBucket } = await supabase.storage.getBucket(bucketName)
     if (errGetBucket && !errGetBucket.message.includes('not found')) {
       core.setFailed(errGetBucket.message + '-' + supabaseUrl)
-      // return
+      return
     }
 
     if (!bucket) {
@@ -30,7 +30,7 @@ async function run() {
       })
       if (errCreateBucket) {
         core.setFailed(errCreateBucket.message)
-        // return
+        return
       }
     }
 
@@ -38,23 +38,23 @@ async function run() {
     const { error: errEmptyBucket } = await supabase.storage.emptyBucket(bucketName)
     if (errEmptyBucket) {
       core.setFailed(errEmptyBucket.message)
-      // return
+      return
     }
 
-    // const filePathDir = isAbsolute(distFolder) ? distFolder : `${process.env.GITHUB_WORKSPACE}/${distFolder}`
-    // filePathDir = '/dist'
-    // core.debug(`Reading file: ${distFolder}`)
-    // core.debug(`Reading file dir: ${filePathDir}`)
+    const filePathDir = isAbsolute(distFolder) ? distFolder : `${process.env.GITHUB_WORKSPACE}/${distFolder}`
+    filePathDir = '/dist'
+    core.debug(`Reading file: ${distFolder}`)
+    core.debug(`Reading file dir: ${filePathDir}`)
 
-    // const files = fs.readdirSync(filePathDir)
-    // for (const file of files) {
-    //   core.debug(`Uploading ${join(filePathDir, file)} to ${bucketName}`)
-    //   const buffer = fs.readFileSync(join(filePathDir, file))
-    //   const { data, error } = await supabase.storage.from(bucketName).upload(file, buffer)
-    //   if (error) throw new Error(error.message)
-    //   core.debug(`Media Key: ${data?.Key}`)
-    //   core.setOutput('result', data?.Key)
-    // }
+    const files = fs.readdirSync(filePathDir)
+    for (const file of files) {
+      core.debug(`Uploading ${join(filePathDir, file)} to ${bucketName}`)
+      const buffer = fs.readFileSync(join(filePathDir, file))
+      const { data, error } = await supabase.storage.from(bucketName).upload(file, buffer)
+      if (error) throw new Error(error.message)
+      core.debug(`Media Key: ${data?.Key}`)
+      core.setOutput('result', data?.Key)
+    }
   } catch (error) {
     core.setFailed(error.message + ' - ' + filePathDir)
   }
